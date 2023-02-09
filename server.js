043fa8853c30aadd37ef019dd9d02385d8fc6e4c
liming <18003554931@163.com>
2023-02-09 11:50:54 +0800

    fix🐛:  修复鼎桥链接不能转换为100%，导致不能自动停止

--- server.js
+++ server.js
@@ -33,65 +33,64 @@ function createServer() {
  * @param req
  */
 function rtspToFlvHandle(ws, req) {
+    let WAITTIME = 8
     let duration = 0
     let t = null
     let command = null
     const stream = webSocketStream(ws, {
         binary: true,
     });
+    const closed = (type) => {
+        if(type) {
+            command._events.close[0]()
+        }
+        if(t) {
+            console.log('已清除')
+            clearInterval(t)
+        }
+        duration = 0
+    }
     const url = new Buffer(req.query.url, "base64").toString();
-        let wucha = req.url.includes('audio') ? 5 : 1.5
-        ffmpeg(url)
-        .ffprobe((err, metadata) => {
-            console.log(metadata.format.duration);
-            command = ffmpeg(url)
-            .on("start", (commandLine) => {
-                // commandLine 是完整的ffmpeg命令
-                console.log(commandLine, "转码 开始");
-            })
-            .on("codecData", function (data) {
-                console.log(data, "转码中......");
-            })
-            .on("progress", function (progress) {
-                duration = 0
-                console.log(progress, "转码进度");
-            })
-            .on("error", function (err) {
-                console.log(url, "转码 错误: ", err);
-            })
-            .on("end", function () {
-                console.log(url, "转码 结束!");
-            })
-            .addOutputOption(
-                "-threads",
-                "4", // 一些降低延迟的配置参数
-                "-tune",
-                "zerolatency",
-                "-preset",
-                "superfast"
-            )
-            .audioCodec("aac")
-            .outputFormat("flv")
-            .videoCodec("libx264") // ffmpeg无法直接将h265转换为flv的，故需要先将h265转换为h264，然后再转换为flv
-            // .withSize('50%') // 转换之后的视频分辨率原来的50%, 如果转换出来的视频仍然延迟高，可按照文档上面的描述，自行降低分辨率
-            //.noAudio() // 去除声音
-            .duration(metadata.format.duration - wucha)
-            .pipe(stream)
+        command = ffmpeg(url)
+        .on("start", (commandLine) => {
+            // commandLine 是完整的ffmpeg命令
+            console.log(commandLine, "转码 开始");
+            t = setInterval(() => {
+                duration += 1
+                if(duration > WAITTIME) {
+                    closed(true)
+                }
+            }, 1000)
         })
-        
-
-        // console.log(3333)
-        // setTimeout(() => {
-        //     console.log(command, 123123)
-        // }, 15000);
-        // t = setInterval(() => {
-        //     duration += 1
-        //     if(duration > 5) {
-        //         clearInterval(t)
-        //         command._events.prefinish()
-        //     console.log(duration, command)
-        //     }
-        // }, 1000)
+        .on("codecData", function (data) {
+            console.log(data, "转码中......");
+        })
+        .on("progress", function (progress) {
+            duration = 0
+            console.log(progress, "转码进度");
+        })
+        .on("error", function (err) {
+            closed()
+            console.log(url, "转码 错误: ", err);
+        })
+        .on("end", function () {
+            closed()
+            console.log(url, "转码 结束!");
+        })
+        .addOutputOption(
+            "-threads",
+            "4", // 一些降低延迟的配置参数
+            "-tune",
+            "zerolatency",
+            "-preset",
+            "superfast"
+        )
+        .audioCodec("aac")
+        .outputFormat("flv")
+        .videoCodec("libx264") // ffmpeg无法直接将h265转换为flv的，故需要先将h265转换为h264，然后再转换为flv
+        // .withSize('50%') // 转换之后的视频分辨率原来的50%, 如果转换出来的视频仍然延迟高，可按照文档上面的描述，自行降低分辨率
+        //.noAudio() // 去除声音
+        .pipe(stream, {end: true})
 
 }
 
